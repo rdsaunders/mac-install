d1b8bdd7d0997ea1f01bc5155511413e796edfe6
Richard Saunders <mail@rdsaunders.co.uk>
2016-03-10 21:22:33 +0000

    Create installConfig.sh

---
+++ installConfig.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Install XCODE Command Line Tools
+xcode-select --install
+
+# Development
+brew install wget
+brew install node
+brew install imagemagick
+brew cask install github-desktop
+brew cask install atom
+brew cask install codekit
+brew cask install cornerstone
+brew cask install vmware-fusion
+brew cask install sublime-text
+brew cask install transmit
+
+# Browsers
+brew cask install firefox
+brew cask install google-chrome
+brew cask install opera
+
+# Social & Communication
+brew cask install hipchat
+brew cask install google-hangouts
+brew cask install skype
+
+# Design & Writing
+brew cask install evernote
+brew cask install imageoptim
+brew cask install sketch
+
+# Utilities
+brew cask install flux
+brew cask install dropbox
+brew cask install 1password
+brew cask install spotify
+brew cask install viscosity
+brew cask install fitbit-connect
+brew cask install google-drive
+brew cask install google-earth
+brew cask install google-photos-backup
