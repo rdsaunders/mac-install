855d8ed0ea5f7095b53984cb9664260646489558
Richard Saunders <mail@rdsaunders.co.uk>
2016-03-16 09:14:22 +0000

    Update installConfig.sh

--- installConfig.sh
+++ installConfig.sh
@@ -1,4 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# 
+# Automation script for setting up a new OSX machine
+# 
+# Some apps don't have a cask and so still need to be installed by hand. These
+# include:
+#
+# - Tweetbot (app store)
+# - Wunderlist (app store)
+#
+# Notes:
+#
+#   If installing full Xcode, it's better to install that first from the app
+#   store before running the bootstrap script. Otherwise, Homebrew can't access
+#   the Xcode libraries as the agreement hasn't been accepted yet.
+#
+# Reading:
+#
+# - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
+# - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
+# - https://news.ycombinator.com/item?id=8402079
+# - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
+
 
 # Install XCODE Command Line Tools
 xcode-select --install
