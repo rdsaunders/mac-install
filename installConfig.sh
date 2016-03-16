4ae9a4091c405d0e11c32af1d5bf506c6c70c101
Richard Saunders <mail@rdsaunders.co.uk>
2016-03-16 08:51:03 +0000

    Added Homebrew Install

--- installConfig.sh
+++ installConfig.sh
@@ -3,6 +3,12 @@
 # Install XCODE Command Line Tools
 xcode-select --install
 
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Check everything is ok with homebrew
+brew doctor
+
 # Development
 brew install wget
 brew install node
