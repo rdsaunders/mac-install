92276cf9aa17272a11d3a33afd449a4de128aa4a
Richard Saunders <mail@rdsaunders.co.uk>
2017-07-12 11:54:05 +0100

    Need to replace plist buddy with putil

--- installConfig.sh
+++ installConfig.sh
@@ -118,5 +118,82 @@ defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 # Enable Develop menu in Safari
 defaults write com.apple.Safari IncludeDevelopMenu 1
+# Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don’t use
+# the Dock to launch apps.
+defaults write com.apple.dock persistent-apps -array
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Stop iTunes from responding to the keyboard media keys
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Hide indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool false
+
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+
+
+# Top right screen corner → Notification Center
+defaults write com.apple.dock wvous-tr-corner -int 12
+defaults write com.apple.dock wvous-tr-modifier -int 0
+
+# Bottom right screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+# Show item info near icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c “Set :DesktopViewSettings:IconViewSettings:showItemInfo true” ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c “Set :FK_StandardViewSettings:IconViewSettings:showItemInfo true” ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c “Set :StandardViewSettings:IconViewSettings:showItemInfo true” ~/Library/Preferences/com.apple.finder.plist
+
+# Show item info to the right of the icons on the desktop
+/usr/libexec/PlistBuddy -c “Set DesktopViewSettings:IconViewSettings:labelOnBottom false” ~/Library/Preferences/com.apple.finder.plist
+
+# Enable snap-to-grid for icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c “Set :DesktopViewSettings:IconViewSettings:arrangeBy grid” ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c “Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid” ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c “Set :StandardViewSettings:IconViewSettings:arrangeBy grid” ~/Library/Preferences/com.apple.finder.plist
+
+# Increase grid spacing for icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c “Set :DesktopViewSettings:IconViewSettings:gridSpacing 100” ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c “Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 100” ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c “Set :StandardViewSettings:IconViewSettings:gridSpacing 100” ~/Library/Preferences/com.apple.finder.plist
+
+# Increase the size of icons on the desktop and in other icon views
+/usr/libexec/PlistBuddy -c “Set :DesktopViewSettings:IconViewSettings:iconSize 48” ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c “Set :FK_StandardViewSettings:IconViewSettings:iconSize 48” ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c “Set :StandardViewSettings:IconViewSettings:iconSize 48” ~/Library/Preferences/com.apple.finder.plist
 
 echo "Automation complete"
