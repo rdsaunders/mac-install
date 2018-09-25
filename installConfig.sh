f46ee61a4dd1958a20409da5910a15ec4684ad7d
Richard Saunders <mail@rdsaunders.co.uk>
2018-09-25 09:24:10 +0100

    Update installConfig.sh

--- installConfig.sh
+++ installConfig.sh
@@ -107,6 +107,7 @@ code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension file-icons.file-icons
 code --install-extension mdickin.markdown-shortcuts
 code --install-extension wayou.vscode-todo-highlight
+code --install-extension mrmlnc.vscode-duplicate
 
 # Remove shadows from screenshots
 defaults write com.apple.screencapture disable-shadow -bool true
