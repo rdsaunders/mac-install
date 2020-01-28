4a94e4aa4d119e8034d540194d74bbd8446cfe86
Richard Saunders <mail@rdsaunders.co.uk>
2020-01-28 09:31:23 +0000

    Create save-hotkeys.sh

---
+++ save-hotkeys.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# save-hotkeys.sh
+
+DESTFILE=~/Dropbox/install-hotkeys.sh
+echo '#!/bin/bash' > $DESTFILE
+
+defaults find NSUserKeyEquivalents | sed -e "s/Found [0-9]* keys in domain '\\([^']*\\)':/defaults write \\1 NSUserKeyEquivalents '/" -e "s/    NSUserKeyEquivalents =     {//"  -e "s/};//" -e "s/}/}'/" >> $DESTFILE
+echo killall cfprefsd >> $DESTFILE
+chmod a+x $DESTFILE
