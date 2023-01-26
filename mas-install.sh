92a74826e8f764b39dd85a928a283466a926906f
Richard Saunders <mail@rdsaunders.co.uk>
2023-01-26 11:01:01 +0000

    Update mas-install.sh

--- mas-install.sh
+++ mas-install.sh
@@ -9,12 +9,11 @@ echo "You'll need to open the Mac App Store and ensure you are logged in"
 read -p "Press [Enter] key to start install..."
 
 # install macos apps
-mas install 1333542190 # 1Password 7 (7.2.1)
-mas install 1254981365 # Contrast (1.1)
-mas install 497799835  # Xcode (10.0)
-mas install 1091189122 # Bear (1.6.2)
-mas install 409183694  # Keynote (8.2)
-mas install 1278508951 # Trello (2.10.7)
-mas install 775737590  # iA Writer (5.1.2)
-mas install 442168834  # SiteSucker (2.11.8)
-mas install 585829637  # Todoist (7.1)
+mas install 409183694  # Keynote (12.2.1)
+mas install 405399194  # Kindle (1.39.0)
+mas install 409203825  # Numbers (12.2.1)
+mas install 775737590  # iA Writer (6.0.4)
+mas install 1289583905 # Pixelmator Pro (3.2.3)
+mas install 1450640351 # Lightweight PDF (1.0.5)
+mas install 1254981365 # Contrast (1.3.2)
+mas install 411643860  # DaisyDisk (4.24)
