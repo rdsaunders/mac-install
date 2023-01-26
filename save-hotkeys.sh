bfc305a0ae555c336e6cddc0cb84177d2f4924c5
Richard Saunders <mail@rdsaunders.co.uk>
2023-01-26 11:35:22 +0000

    Update save-hotkeys.sh
    Update Dropbox destination path for Ventura

--- save-hotkeys.sh
+++ save-hotkeys.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # save-hotkeys.sh
 
-DESTFILE=~/Dropbox/install-hotkeys.sh
+DESTFILE=~/Library/CloudStorage/Dropbox/install-hotkeys.sh
 echo '#!/bin/bash' > $DESTFILE
 
 defaults find NSUserKeyEquivalents | sed -e "s/Found [0-9]* keys in domain '\\([^']*\\)':/defaults write \\1 NSUserKeyEquivalents '/" -e "s/    NSUserKeyEquivalents =     {//"  -e "s/};//" -e "s/}/}'/" >> $DESTFILE
