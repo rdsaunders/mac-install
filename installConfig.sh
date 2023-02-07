082d16582898529b1bb607afcb5568a39db47b27
Richard Saunders <mail@rdsaunders.co.uk>
2023-02-07 20:44:52 +0000

    Update installConfig.sh

--- installConfig.sh
+++ installConfig.sh
@@ -35,7 +35,8 @@ brew install wget
 # Language
 ## Node / Javascript
 mkdir ~/.nvm
-brew install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.zshrc
 nvm install node
 brew install yarn  
 
