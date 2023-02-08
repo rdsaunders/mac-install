da120cc5b0df25454bb7604b03d6291ab1fd5735
Richard Saunders <mail@rdsaunders.co.uk>
2023-02-08 11:59:52 +0000

    Update mac-settings.sh

--- mac-settings.sh
+++ mac-settings.sh
@@ -37,7 +37,7 @@ defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
 # Finder | Desktop > View options | Show icon preview
 /usr/libexec/PlistBuddy -c "Set :\"DesktopViewSettings:IconViewSettings\":showIconPreview true" ~/Library/Preferences/com.apple.finder.plist
 # Finder | Desktop > View options | Stack by Kind
-/usr/libexec/PlistBuddy -c "Add :\"DesktopViewSettings\":GroupBy Kind" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Add :\"DesktopViewSettings\":GroupBy kind" ~/Library/Preferences/com.apple.finder.plist
 # Finder | Desktop > View options | Sort by Date Added
 /usr/libexec/PlistBuddy -c "Set :\"DesktopViewSettings:IconViewSettings\":arrangeBy dateAdded" ~/Library/Preferences/com.apple.finder.plist
 
