d696c05a3fdd5a7825d03d218a7befeb5fd20624
Richard Saunders <mail@rdsaunders.co.uk>
2023-02-09 07:02:23 +0000

    Update mac-dock.sh

--- mac-dock.sh
+++ mac-dock.sh
@@ -1,22 +1,53 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-# Specify User Password
-#------------------------------------------------
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+# DESCRIPTION
+# This script configures users docks using docktutil
+# source dockutil https://github.com/kcrawford/dockutil/
+# 
+# REQUIREMENTS
+# dockutil Version 3.0.0 or higher installed to /usr/local/bin/
+# Compatible with macOS 11.x and higher
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-sudo -v
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+# COLLECT IMPORTANT USER INFORMATION
+# Get the currently logged in user
+currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+
+# Get uid logged in user
+uid=$(id -u "${currentUser}")
+
+# Current User home folder - do it this way in case the folder isn't in /Users
+userHome=$(dscl . -read /users/${currentUser} NFSHomeDirectory | cut -d " " -f 2)
 
 # Path to plist
 plist="${userHome}/Library/Preferences/com.apple.dock.plist"
+
+# Convenience function to run a command as the current user
+# usage: runAsUser command arguments...
+runAsUser() {  
+	if [[ "${currentUser}" != "loginwindow" ]]; then
+		launchctl asuser "$uid" sudo -u "${currentUser}" "$@"
+	else
+		brew install jq
+    DLURL=$(curl --silent "https://api.github.com/repos/kcrawford/dockutil/releases/latest" | jq -r '.assets[].browser_download_url' | grep pkg)
+    curl -sL ${DLURL} -o /tmp/dockutil.pkg
+    sudo installer -pkg "/tmp/dockutil.pkg" -target /
+    rm /tmp/dockutil.pkg
+		
+	fi
+}
 
 # Check if dockutil is installed
 if [[ -x "/usr/local/bin/dockutil" ]]; then
     dockutil="/usr/local/bin/dockutil"
 else
-    brew install jq
-    DLURL=$(curl --silent "https://api.github.com/repos/kcrawford/dockutil/releases/latest" | jq -r '.assets[].browser_download_url' | grep pkg)
-    curl -sL ${DLURL} -o /tmp/dockutil.pkg
-    sudo installer -pkg "/tmp/dockutil.pkg" -target /
-    rm /tmp/dockutil.pkg
+    echo "dockutil not installed in /usr/local/bin, exiting"
+    exit 1
 fi
 
 # Version dockutil
@@ -24,33 +55,45 @@ dockutilVersion=$(${dockutil} --version)
 echo "Dockutil version = ${dockutilVersion}"
 
 # Create a clean Dock
-"${dockutil}" --remove all --no-restart ${plist}
+runAsUser "${dockutil}" --remove all --no-restart ${plist}
 echo "clean-out the Dock"
 
 # Full path to Applications to add to the Dock
 apps=(
-"/System/Applications/Calendar.app"
+"/Applications/Self Service.app"
+"/System/Applications/Launchpad.app"
+"/Applications/zoom.us.app"
 "/Applications/Google Chrome.app"
 "/Applications/Safari.app"
+"/System/Applications/Calendar.app"
+"/System/Applications/Notes.app"
+"/Applications/Keynote.app"
+"/Applications/Numbers.app"
+"/Applications/Pages.app"
+"/System/Applications/System Preferences.app"
 )
 
 # Loop through Apps and check if App is installed, If Installed at App to the Dock.
 for app in "${apps[@]}"; 
 do
 	if [[ -e ${app} ]]; then
-		"${dockutil}" --add "$app" --no-restart ${plist};
+		runAsUser "${dockutil}" --add "$app" --no-restart ${plist};
 	else
 		echo "${app} not installed"
     fi
 done
 
+# Add Application Folder to the Dock
+runAsUser "${dockutil}" --add /Applications --view grid --display folder --sort name --no-restart ${plist}
+
+# Add logged in users Downloads folder to the Dock
+runAsUser "${dockutil}" --add ${userHome}/Downloads --view list --display stack --sort dateadded --no-restart ${plist}
+
+# Disable show recent
+# runAsUser defaults write com.apple.dock show-recents -bool FALSE
+# echo "Hide show recent from the Dock"
+
 # sleep 3
-
-
-# Cleanup
-#------------------------------------------------
-brew uninstall jq
-
 
 # Kill dock to use new settings
 killall -KILL Dock
@@ -58,22 +101,4 @@ echo "Restarted the Dock"
 
 echo "Finished creating default Dock"
 
-# Remove all items from the Dock
-#------------------------------------------------
-#dockutil --remove all
-
-# Add dock items
-#------------------------------------------------
-# Messages
-#Calendar
-#Slack
-#Notion
-#--
-#Firefox
-#Chrome
-#Safari
-#--
---
-##Visual Studio Code
-#Figma
-#Music
+exit 0
